d223d1834a60bf8b329b0db5b384cf2dcbb88c3e
malle-pietje <erik@slooff.net>
2015-12-20 19:38:24 +0100

    updated instruction in "config.php" to make sure a value of at least 4.0.0 is entered for $controllerversion
    when no value is entered for $controllerversion the initial page load will show an empty site selection list

--- config.template.php
+++ config.template.php
@@ -26,6 +26,6 @@ THE SOFTWARE.
 $controlleruser     = ''; // the user name for access to the Unifi Controller
 $controllerpassword = ''; // the password for access to the Unifi Controller
 $controllerurl      = ''; // full url to the Unifi Controller, eg. 'https://22.22.11.11:8443'
-$controllerversion  = ''; // the version of the Controller software, eg. '4.6.6', this value is not yet used
+$controllerversion  = ''; // the version of the Controller software, eg. '4.6.6' (must be at least 4.0.0)
 
 ?>
