2eb02f249a54bfe1d3738ca49543b6c31b026dde
Klint Van Tassel <github@sprocktech.com>
2018-11-19 19:21:18 -0600

    updated README and config template

--- config.template.php
+++ config.template.php
@@ -8,6 +8,12 @@
  */
 
 /**
+ * Optionally copy/rename this configuration template file to config.php and store all or part of your credentials
+ * If credential information is not specific in this file, or the file is not copied to config.php,
+ * or only part of the crential information is given, then the API browser will show a form to complete the login
+ */
+
+/**
  * Single controller configuration
  * ===============================
  * Update this section with your UniFi controller details and credentials
@@ -16,6 +22,7 @@
 $controlleruser     = ''; // the user name for access to the UniFi Controller
 $controllerpassword = ''; // the password for access to the UniFi Controller
 $controllerurl      = ''; // full url to the UniFi Controller, eg. 'https://22.22.11.11:8443'
+$controllername     = ''; // name for this controller
 
 /**
  * Multi controller configuration
@@ -25,27 +32,30 @@ $controllerurl      = ''; // full url to the UniFi Controller, eg. 'https://22.2
  * index values (0,1,2 etc.)
  *
  * Please remember to only have one of either two methods active!
+ *
+ * NOTE: If you use the multi controller method, then at least the 'name' value is required
+ *
  */
 /*
 $controllers[0] = [
    'user'     => '', // the user name for access to the UniFi Controller
    'password' => '', // the password for access to the UniFi Controller
    'url'      => '', // full url to the UniFi Controller, eg. 'https://22.22.11.11:8443'
-   'name'     => ''  // name for this controller which will be used in the dropdown menu
+   'name'     => ''  // REQUIRED: name for this controller which will be used in the dropdown menu
 ];
 
 $controllers[1] = [
    'user'     => '', // the user name for access to the UniFi Controller
    'password' => '', // the password for access to the UniFi Controller
    'url'      => '', // full url to the UniFi Controller, eg. 'https://22.22.11.11:8443'
-   'name'     => ''  // name for this controller which will be used in the dropdown menu
+   'name'     => ''  // REQUIRED: name for this controller which will be used in the dropdown menu
 ];
 
 $controllers[2] = [
    'user'     => 'demo', // the user name for access to the Unifi Controller
    'password' => 'demo', // the password for access to the Unifi Controller
    'url'      => 'https://demo.ubnt.com:443', // full url to the Unifi Controller, eg. 'https://22.22.11.11:8443'
-   'name'     => 'demo.ubnt.com'
+   'name'     => 'demo.ubnt.com' // REQUIRED: name for this controller which will be used in the dropdown menu
 ];
 */
 
