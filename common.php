546a800756b1c056eedf98da4cc58f44f20a9c3d
malle-pietje <erik@slooff.net>
2024-09-14 17:42:05 +0200

    bumped version to 2.0.28

--- common.php
+++ common.php
@@ -10,7 +10,7 @@ require_once 'vendor/autoload.php';
 
 use UniFi_API\Client as ApiClient;
 
-const TOOL_VERSION = '2.0.27';
+const TOOL_VERSION = '2.0.28';
 
 /**
  * Gather some basic information for the About modal.
